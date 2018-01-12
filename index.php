e7228655d5d5b9a3cffc233eb5f0fb72c9e1218e
Nolan Medina <njmedina614@gmail.com>
2018-01-12 14:50:24 -0500

    added 3 routes

--- index.php
+++ index.php
@@ -7,12 +7,47 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 
+$f3 -> set('DEBUG', 3);
+//define a page1 route
+
+
 $f3->route('GET /', function() {
 
     //$views = new View;
     //echo $views->render ('views/home.html');
 
     echo '<h1>Routing Demo</h1>';
+
+}
+);
+
+
+$f3->route('GET /page1', function() {
+
+    //$views = new View;
+    //echo $views->render ('views/home.html');
+
+    echo '<h1>This is Page1</h1>';
+
+}
+);
+
+$f3->route('GET /page2', function() {
+
+    //$views = new View;
+    //echo $views->render ('views/home.html');
+
+    echo '<h1>i noe de wey</h1>';
+
+}
+);
+
+$f3->route('GET /page1/subpage-a', function() {
+
+    //$views = new View;
+    //echo $views->render ('views/home.html');
+
+    echo '<h1>*clucking*</h1>';
 
 }
 );
